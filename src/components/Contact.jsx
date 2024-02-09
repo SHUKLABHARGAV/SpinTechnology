16ca5ea4c21a08b59949d014e8c47d4576c15b9a
SHUKLABHARGAV <88668110+SHUKLABHARGAV@users.noreply.github.com>
2024-02-09 11:57:23 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -46,8 +46,9 @@ const Contact = () => {
             </div>
             <div className="row mb-5">
                 <div className="col-12 wow fadeInUp" data-wow-delay="0.1s">
+                  
                     <iFrame className="w-100"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1840.0139888903848!2d71.66306351244059!3d22.727201460314834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395941007401fc4d%3A0x219c1cbdf7b0dbc9!2sSpin%20Technology!5e0!3m2!1sen!2sin!4v1707459896944!5m2!1sen!2sin"
                         frameborder="0" style={{minHeight: "450px;", border:"0;"}} allowFullScreen="" aria-hidden="false"
                         tabindex="0"></iFrame>
                 </div>
